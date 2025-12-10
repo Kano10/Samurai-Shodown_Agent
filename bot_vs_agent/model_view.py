213006d24eae4d3603e0f7d78341183641a9dd80
Luciano <luargomedo2021@inf.udec.cl>
2025-12-10 20:21:24 -0300

    agente listo creo

--- bot_vs_agent/model_view.py
+++ bot_vs_agent/model_view.py
@@ -5,29 +5,33 @@ from ss_env_Bot import SamuraiShodownEnv
 import time
 
 # === Parámetros ===
-model_path = "./RL/ppo_SS_model1" 
-max_steps = 10_000                 # Num de pasos a ver
+model_path = "./RL/ppo_SS_model1"
+max_steps = 10_000
 
 # === Carga el entorno y el modelo ===
 env = SamuraiShodownEnv()
 model = PPO.load(model_path)
 obs, _ = env.reset()
-print("Modelo Cargado")
+print("Modelo cargado")
 reca = 0
 
 # === Loop de visualización ===
 for step in range(max_steps):
     action, _ = model.predict(obs, deterministic=True)
-    #print("Inicio de step")
-    obs, reward, done, _, info = env.step(action)
+
+    # Nuevo formato Gymnasium
+    obs, reward, terminated, truncated, info = env.step(action)
+    done = terminated or truncated
+
     reca += reward
     print(reca)
-    env.render()
-    #print("Step hecho")
 
-    # Si termina el episodio, reseteamos
+    # Retro ya renderiza automáticamente
+    time.sleep(0.02)
+
     if done:
         reca = 0
         obs, _ = env.reset()
 
 env.close()
+
