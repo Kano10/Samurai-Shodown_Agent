28214f4998f6144afab9d0d3f29e8926426c7049
Kano10 <canomga@gmail.com>
2025-12-15 12:41:09 -0300

    lol

---
+++ bot_vs_agente4/reward_logger_callback_Bot.py
@@ -0,0 +1,52 @@
+from stable_baselines3.common.callbacks import BaseCallback
+from torch.utils.tensorboard import SummaryWriter
+
+class RewardLoggerCallback(BaseCallback):
+    def __init__(self, verbose=0):
+        super().__init__(verbose)
+        self.writer = None
+        self.episode_rewards = []
+
+    def _on_training_start(self):
+        self.writer = SummaryWriter(log_dir=self.logger.dir)
+
+    def _on_step(self):
+        rewards = self.locals['rewards']
+        dones = self.locals['dones']
+        infos = self.locals['infos']
+
+        for i in range(len(rewards)):
+            if len(self.episode_rewards) <= i:
+                self.episode_rewards.append(0.0)
+
+            self.episode_rewards[i] += rewards[i]
+
+            if dones[i]:
+                info = infos[i]
+                efective_attack_steps = info.get("efective_attack_steps", 0)
+                total_steps = max(info.get("total_steps", 1), 1)
+                damage_to_player = info.get("damage_to_player_steps", 0)
+
+                # Porcentajes
+                pct_attack = efective_attack_steps / total_steps
+                pct_block = efective_block_steps / total_steps
+                pct_dano = damage_to_player / total_steps
+
+                # Logging en TensorBoard: Porcentajes
+                self.writer.add_scalar("episode/reward", self.episode_rewards[i], self.num_timesteps)
+                self.writer.add_scalar("episode/pct_attack", pct_attack, self.num_timesteps)
+                self.writer.add_scalar("episode/pct_block", pct_block, self.num_timesteps)
+                self.writer.add_scalar("episode/pct_dano", pct_dano, self.num_timesteps)
+
+                # Logging en TensorBoard: Valores absolutos
+                self.writer.add_scalar("raw/attack_steps", efective_attack_steps, self.num_timesteps)
+                self.writer.add_scalar("raw/damage_to_player", damage_to_player, self.num_timesteps)
+                self.writer.add_scalar("raw/total_steps", total_steps, self.num_timesteps)
+
+                # Reset de reward
+                self.episode_rewards[i] = 0.0
+
+        return True
+
+    def _on_training_end(self):
+        self.writer.close()
